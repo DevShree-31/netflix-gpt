befaac8c139db2860c20bac5fc13f2197c6275bc
Shreyansh <ui21cs56@iiitsurat.ac.in>
2024-04-09 12:10:04 +0530

    removed the error

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -16,7 +16,7 @@ const VideoBackground = ({movieId}) => {
   return (
     <div>
       <iframe width="560" height="315" 
-      src={"https://www.youtube.com/embed/" +trailer.key}
+      src="https://www.youtube.com/embed/V5-7GzOfADQ?si=EeDbnyPgmX-LFKmW" 
       title="YouTube video player" 
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
       referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
